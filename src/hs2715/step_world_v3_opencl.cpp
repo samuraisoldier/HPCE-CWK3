9edad01e9bab199c06120cc552971129252312b0
samuraisoldier <24753407+samuraisoldier@users.noreply.github.com>
2019-02-09 18:55:55 +0000

    maybe fixed v3

--- src/hs2715/step_world_v3_opencl.cpp
+++ src/hs2715/step_world_v3_opencl.cpp
@@ -135,7 +135,7 @@ void StepWorldV3OpenCL(world_t &world, float dt, unsigned n)
 	
 	cl::Context context(devices);
 	
-	std::string kernelSource=hpce::hs2715::LoadSource("step_world_v3_kernel.cl");
+	std::string kernelSource=LoadSource("step_world_v3_kernel.cl");
 
 	cl::Program::Sources sources;	// A vector of (data,length) pairs
 	sources.push_back(std::make_pair(kernelSource.c_str(), kernelSource.size()+1));	// push on our single string
